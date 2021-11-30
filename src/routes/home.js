1c90b9fb5f68335ebe223c6acc82e04eb15d28d0
Junhyun <mnmthatisus@gmail.com>
2021-11-30 21:46:31 +0900

    ??

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,57 @@
-const Home = ()=>{
-    return <h1>HoME</h1>
-}
+import {useEffect, useState} from 'react';
+import { dbService } from '../firebase';
 
+const Home = () => {
+  const [twitter, setTwitter] = useState('');
+  const [allTwitts,setAllTweets]=useState([])
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    await dbService.collection("Tweets").add({
+        twitter,
+        createdAt:Date.now()
+    })    
+    setTwitter("")
+  };
+  const handleChange = (e) => {
+    const {value} = e.target;
+    setTwitter(value);
+  };
+
+  const getAllTweets=async()=>{
+    const response = await dbService.collection("Tweets").get();
+    response.forEach(item=>{
+        const newObj = {
+            ...item.data(),
+            id:item.id
+        }
+        setAllTweets(prev=>[...prev,newObj])
+    });
+  }
+
+  useEffect(()=>{
+    getAllTweets();
+  },[])
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={twitter}
+          onChange={handleChange}
+          placeholder="what's on your mind ?"
+          maxLength={20}
+        />
+        <input type="submit" value="twitter" />
+      </form>
+      <section>
+        {allTwitts.map(item=>{
+            const {id,twitter}=item;
+            return <div key={id}>
+                <h4>{twitter}</h4>
+            </div>
+        })}
+      </section>
+    </>
+  );
+};
 export default Home;
